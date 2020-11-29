2fd2f86381b115f474a8e9fc464a424e1bbd5a26
Gabriel Rodriguez <garodriguezm96@gmail.com>
2020-11-29 03:07:20 -0300

    Style: Cambios para el posterior responsive Se han modificado los css para la reutilizacion de clases y ademas organizar las estructuras en grid para el posterior responsive

--- src/app/services/localData/local-data.service.ts
+++ src/app/services/localData/local-data.service.ts
@@ -9,7 +9,18 @@ export class LocalDataService {
 
   constructor() { }
 
-  private _data: Transaction[]
+  private _data: Transaction[] = [
+    { id: 1, concept: "Netflix", amount: 5, transactionDate: "2011-08-23", type: Type.WITHDRAWALS, creationDate: "2020-09-23-05-10-06" },
+    { id: 2, concept: "Sueldo", amount: 10, transactionDate: "2011-09-23", type: Type.DEPOSIT, creationDate: "2020-09-24-05-10-06" },
+    { id: 3, concept: "Netflix", amount: 5, transactionDate: "2011-08-23", type: Type.WITHDRAWALS, creationDate: "2020-09-23-05-10-07" },
+    { id: 4, concept: "Sueldo", amount: 10, transactionDate: "2011-09-23", type: Type.DEPOSIT, creationDate: "2020-09-24-05-10-08" },
+    { id: 5, concept: "Netflix", amount: 5, transactionDate: "2011-08-23", type: Type.WITHDRAWALS, creationDate: "2020-09-23-05-10-09" },
+    { id: 6, concept: "Sueldo", amount: 10, transactionDate: "2011-09-23", type: Type.DEPOSIT, creationDate: "2020-09-24-05-10-06" },
+    { id: 7, concept: "Netflix", amount: 5, transactionDate: "2011-08-23", type: Type.WITHDRAWALS, creationDate: "2020-09-23-05-10-10" },
+    { id: 8, concept: "Sueldo", amount: 10, transactionDate: "2011-09-23", type: Type.DEPOSIT, creationDate: "2020-09-24-05-10-11" },
+    { id: 9, concept: "Netflix", amount: 5, transactionDate: "2011-08-23", type: Type.WITHDRAWALS, creationDate: "2020-09-23-05-10-12" },
+    { id: 10, concept: "Sueldo", amount: 10, transactionDate: "2011-09-23", type: Type.DEPOSIT, creationDate: "2020-09-24-05-10-13" },
+  ]
 
   public get data(): Transaction[] {
     return this._data;
--- src/app/views/user/budget-list/budget-list.component.ts
+++ src/app/views/user/budget-list/budget-list.component.ts
@@ -60,7 +60,6 @@ export class BudgetListComponent implements AfterViewInit {
         this.updateDatas();
 
       }
-
     });
   }
 
